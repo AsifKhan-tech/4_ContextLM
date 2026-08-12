529714f466f3a03012c0c247b1337135f618db83
Asif Khan <asifjamshed4@gmail.com>
2026-08-12 15:56:01 +0530

    feat: integrate better-auth for authentication.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 8000;
+
+/*
+ * Better Auth handler
+ */
+app.all("/api/auth/{*any}", toNodeHandler(auth));
+
+/*
+ * Mount express json middleware after Better Auth handler
+ * or only apply it to routes that don't interact with Better Auth
+ */
+
+app.use(express.json());
 
 app.get("/health", (req, res) => {
   return res.status(200).json({
